6cab97256f11fe8ce84841b470d80432b9d0113b
BeeFriedman <70110787+BeeFriedman@users.noreply.github.com>
2021-09-24 00:19:39 -0400

    added a function to create the event listener

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-var userInput = document.getElementById("user-input");
+//when the page is finished loading it prints the first line
+onload = () => {
+    createNewUserInputLine();
+}
 
-userInput.addEventListener("keyup", event => {
-    if (event.key === "Enter" || event.keyCode === 13) {
-        event.preventDefault();
+//adds an event listener for the newest input line to capture when 
+//the user clicks the enter key and removes the listener and disables
+//the input from the last one 
+function addListenerForUserInput() {
+    var userInputArray = document.getElementsByClassName("user-input");
+    var activeUserInput = userInputArray[userInputArray.length - 1];
+    var unactiveUserInput = userInputArray[userInputArray.length - 2];
 
-        switch(userInput.value.toLowerCase()){
-            case "help":
-                displayCommands();
-                break;
+    function captureEnterPress(event) {
+        if (event.key === "Enter" || event.keyCode === 13) {
+            event.preventDefault();
+
+            switch(activeUserInput.value.toLowerCase().trim()){
+                case "help":
+                    displayCommands();
+                    break;
+            }
         }
     }
-});
 
+    if(userInputArray.length > 1){
+        unactiveUserInput.removeEventListener("keyup", captureEnterPress);
+        unactiveUserInput.disabled = true;
+    }
+
+    activeUserInput.focus();
+    activeUserInput.addEventListener("keyup", captureEnterPress);
+}
+
+//creates the container, text and input for the new line
 function createNewUserInputLine() {
     var body = document.body;
     var inputLine = document.createElement("div");
@@ -25,8 +46,10 @@ function createNewUserInputLine() {
     inputLine.appendChild(inputLineLabel);
     inputLine.appendChild(inputLineTextbox);
     body.appendChild(inputLine);
+    addListenerForUserInput();
 }
 
+//prints out the output according to the users input
 function displayCommands() {
     var element = document.body;
     var br = document.createElement("br");
@@ -34,7 +57,7 @@ function displayCommands() {
     var outputLineText = document.createTextNode(`Type "ls" to list all available directories`);
     outputLine.className = "output-line";
     outputLine.appendChild(outputLineText);
-    element.appendChild(br);
     element.appendChild(outputLine);
+    element.appendChild(br);
     createNewUserInputLine();
 }
